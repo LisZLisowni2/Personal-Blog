ff583b3a7a167d2b8f6fe9ed2f75bbc1e7ef6d71
LisZLisowni <rafalkozikowski735@gmail.com>
2024-10-31 19:23:53 +0100

    Add register route

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const user = require('models/user')
 const post = require('models/post')
 const app = express()
@@ -13,6 +15,17 @@ app.use(express.json())
 mongoose.connect(db_uri)
 .then(() => console.log("Connection established!"))
 .catch(err => console.error(err))
+
+app.post('/register', async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        if (!username || !email || !password) {
+            return res.status(400).json({ 'message': 'Username, email or password not present' })
+        }
+    } catch (err) {
+
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at http://127.0.0.1:${PORT}`)
