de591a94ddb50619182c98deffdd1463b04ef8a7
LisZLisowni <rafalkozikowski735@gmail.com>
2024-10-31 19:05:32 +0100

    Add user and post model, finish user model

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    'username': {
+        type: String,
+        required: true
+    },
+    'email': {
+        type: String,
+        required: true
+    },
+    'password': {
+        type: String,
+        require: true
+    },
+    'scope': {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
+})
