276501286aab57f6301605c774cdc41ac6e920cc
LisZLisowni <rafalkozikowski735@gmail.com>
2024-10-31 19:14:20 +0100

    Finish post model

---
+++ backend/models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    'title': {
+        type: String,
+        required: true
+    },
+    'description': {
+        type: String,
+        required: true
+    },
+    'data': {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    'published': {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+})
+
+const Post = mongoose.Model('Post', postSchema)
+module.exports = Post
