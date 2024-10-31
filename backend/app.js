1d972a03b50e55b487711d8c5a58c6eec2d3920a
LisZLisowni <rafalkozikowski735@gmail.com>
2024-10-31 18:56:37 +0100

    Add listen to app

--- backend/app.js
+++ backend/app.js
@@ -12,3 +12,6 @@ mongoose.connect(db_uri)
 .then(() => console.log("Connection established!"))
 .catch(err => console.error(err))
 
+app.listen(PORT, () => {
+    console.log(`Server running at http://127.0.0.1:${PORT}`)
+})
