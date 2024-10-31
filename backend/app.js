c60db3b7d41aff62e4da0870994fd94819e874f3
LisZLisowni <rafalkozikowski735@gmail.com>
2024-10-31 20:24:48 +0100

    Add comments to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,30 @@
+// Import environment variables
 require('dotenv').config()
 
+// Import packages and models
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('./models/user')
 const Post = require('./models/post')
+// Create express app
 const app = express()
+// Settings
 // SECURITY WARNING! Turn off on production
 const DEBUG = true
 const PORT = process.env.PORT || 3000
 const db_uri = process.env.DATABASE_URI
 
+// Middleware to parse json body
 app.use(express.json())
 
+// Handle a connection about MongoDB and error
 mongoose.connect(db_uri)
 .then(() => console.log("Connection established!"))
 .catch(err => console.error(err))
 
+// Authentication token
 function authenticateToken(req, res, next) {
     const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1]
     if (!token) return res.status(401).json({ 'message': 'Token missing, access denied' })
@@ -29,11 +36,13 @@ function authenticateToken(req, res, next) {
     })
 }
 
+// 500 error handler
 function displayError(res, err) {
     if (!DEBUG) res.status(500).json({ 'message': 'Server error' })
     else res.status(500).json({ 'message': 'Server error', 'error': err.message })
 }
 
+// Register route
 app.post('/register', async (req, res) => {
     try {
         const { username, email, password } = req.body
@@ -49,6 +58,7 @@ app.post('/register', async (req, res) => {
     }
 })
 
+// Login route
 app.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body
@@ -71,6 +81,7 @@ app.post('/login', async (req, res) => {
     }
 })
 
+// Account details
 app.get('/user', authenticateToken, async (req, res) => {
     try {
         const username = req.user.username
