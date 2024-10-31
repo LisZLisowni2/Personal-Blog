689f0c7d96c174831037c3a258d34d5565c155c4
LisZLisowni <rafalkozikowski735@gmail.com>
2024-10-31 18:52:23 +0100

    Update a db connection

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
+const db_uri = process.env.DATABASE_URI
 
 app.use(express.json())
 
-mongoose.connect(process.env.DATABASE_URI)
+mongoose.connect(db_uri)
 .then(() => console.log("Connection established!"))
 .catch(err => console.error(err))
 
