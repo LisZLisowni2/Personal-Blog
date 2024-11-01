ec7a6d4e037ede150f3004e56e2956165eff721e
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-01 14:00:02 +0100

    Create post list component

---
+++ frontend/src/components/Posts/postslist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Content() {
+    return (
+        <div className="grid grid-cols-6">
+            <div className="bg-orange-400"></div>
+            <div className="col-span-4 bg-emerald-400"></div>
+            <div className="bg-orange-400"></div>
+        </div>
+    )
+}
+
+export default Content
