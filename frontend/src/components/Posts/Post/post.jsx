80e36ef042dcf96e2bab8c5a21af09b059a7ee8e
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-01 18:56:20 +0100

    Link to details of post

--- frontend/src/components/Posts/Post/post.jsx
+++ frontend/src/components/Posts/Post/post.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useParams, useNavigate } from "react-router-dom"
 
 export default function Post(request) {
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const post = request.posts.find(item => item._id === id)
+
     return (
         <div>
-            <span>{ request.title }</span>
-            <span>{ request.date }</span>
-            <span>{ request.description }</span>
+            <span>{ post.title }</span>
+            <span>{ post.date }</span>
+            <span>{ post.description }</span>
         </div>
     )
 }
--- frontend/src/components/Posts/Post/postCard.jsx
+++ frontend/src/components/Posts/Post/postCard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const PostCard = request => {
     return (
-        <div className="border-2 border-black p-4 mb-4 last:mb-0 max-h-52 overflow-hidden">
+        <Link to={`/${request.id}`}>
+        <div className="border-2 border-black p-4 m-4 max-h-52 overflow-hidden">
             <span className="text-2xl">{ request.title }</span><br />
             <span className="text-opacity-70">{ request.date }</span>
             <hr className="border-black my-4"/>
             <span>{ request.description }</span>
         </div>
+        </Link>
     )
 }
 
