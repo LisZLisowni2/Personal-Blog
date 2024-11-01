5fa7f4ddc7d691aebf671daf5caebfa53be4d14a
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-01 13:21:40 +0100

    Apply NavBar to App.jsx and edit NavBar comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar/navbar'
 
 function App() {
   return (
     <>
-      
+      <NavBar />
     </>
   )
 }
--- frontend/src/components/NavBar/navbar.jsx
+++ frontend/src/components/NavBar/navbar.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+function NavBar() {
+    return (
+        <div className="h-1/6 bg-rose-600 p-4 grid grid-cols-2">
+            <div class="col-span-2"><h1>LisZLisowni blog</h1></div>
+            <div class="col-span-1"><h2>Ikonki</h2></div>
+        </div>
+    )
+}
+
+export default NavBar;
