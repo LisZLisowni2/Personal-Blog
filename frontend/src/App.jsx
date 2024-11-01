d73002827876332f261ba6de09b421f6a08f728c
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-01 14:02:12 +0100

    Update content component style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar/navbar'
+import Content from './components/Posts/postslist'
 
 function App() {
   return (
     <>
       <NavBar />
+      <Content />
     </>
   )
 }
--- frontend/src/components/Posts/postslist.jsx
+++ frontend/src/components/Posts/postslist.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Content() {
     return (
-        <div className="grid grid-cols-6">
+        <div className="grid grid-cols-6 h-full">
             <div className="bg-orange-400"></div>
-            <div className="col-span-4 bg-emerald-400"></div>
+            <div className="col-span-4 bg-emerald-400 p-4">
+                POST
+            </div>
             <div className="bg-orange-400"></div>
         </div>
     )
