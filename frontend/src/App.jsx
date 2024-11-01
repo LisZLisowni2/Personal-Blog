8428fe84f613295114b6b0c9570cc9986056f1c8
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-01 17:58:13 +0100

    Import axios and fetch a posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import NavBar from './components/NavBar/navbar'
 import Content from './components/Posts/postslist'
 import Footer from './components/Footer/footer'
+import axios from 'axios'
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = axios.get("http://127.0.0.1:3000/posts/")
+    setPosts(fetchPosts)
+  }, [])
+
   return (
     <div className="flex flex-col h-screen">
       <NavBar />
