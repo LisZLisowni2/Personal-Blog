9fc4c90ceeaa71f17039d94dc60c51ee0b548387
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-02 13:19:18 +0100

    Add register component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import PostList from './components/Posts/postslist'
 import Post from './components/Posts/Post/post'
 import Footer from './components/Footer/footer'
 import LoginForm from './components/User/login'
+import RegisterForm from './components/User/register'
 import { PostProvide } from './context/PostContext'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
@@ -17,6 +18,7 @@ function App() {
               <Route path='/' element={<PostList/>} />
               <Route path='/:id' element={<Post/>} />
               <Route path='/login' element={<LoginForm />} />
+              <Route path='/register' element={<RegisterForm />}/>
             </Routes>
           <Footer className="content-end"/>
         </div>
---
+++ frontend/src/components/User/register.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Content from "../Content/Content";
+import { Link } from "react-router-dom"
+
+function LoginForm() {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <Content>
+            <form method="POST" className="flex flex-col border-black border rounded w-1/2 text-center justify-center p-4 m-auto *:mb-4">
+                <label className="block font-bold text-xl" for="username">
+                    Username
+                </label>
+                <input id="username" type="text" placeholder="Username" className="text-center border-sky-400 border-2 p-2 w-1/2 m-auto"/>
+                <label className="block font-bold text-xl" for="password">
+                    Email
+                </label>
+                <input id="password" type="password" placeholder="Password" className="text-center border-sky-400 border-2 p-2 w-1/2 m-auto"/>
+                <label className="block font-bold text-xl" for="password">
+                    Password
+                </label>
+                <input id="password" type="password" placeholder="Password" className="text-center border-sky-400 border-2 p-2 w-1/2 m-auto"/>
+                <div className="flex items-center *:mr-16">
+                    <button className="bg-blue-500 hover:bg-blue-700 active:bg-blue-900 p-4 w-1/4 m-auto rounded text-white">Register</button>
+                    <Link to="/login" className="hover:text-gray-700 active:text-gray-400">Have account? Login</Link>
+                </div>
+            </form>
+        </Content>
+    )
+}
+
+export default LoginForm
