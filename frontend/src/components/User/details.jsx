57388a46b7975bd38b724c8b5d91d4b54b5b1643
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-03 12:26:47 +0100

    Update details.jsx for logout

--- frontend/src/components/User/details.jsx
+++ frontend/src/components/User/details.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Content from "../Content/Content";
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useUser } from "../../context/UserContext";
 
 function UserDetails() {
     const { user, loading, handleUserLogout } = useUser()
+    const navigate = useNavigate()
 
     if (loading) {
         return (
@@ -22,12 +23,17 @@ function UserDetails() {
         )
     }
 
+    const handleLogout = async () => {
+        await handleUserLogout()
+        navigate("/")
+    }
+
     return (
         <Content>
             <span>Username: { user.username }</span>
             <span>Email: { user.email }</span>
             <span>Role: { user.scope }</span>
-            <button onClick={handleUserLogout}>Logout</button>
+            <button onClick={handleLogout}>Logout</button>
         </Content>
     )
 }
