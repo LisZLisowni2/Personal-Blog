650ea52df4ca73546a979087e47c6e5743fe7c0b
LisZLisowni <rafalkozikowski735@gmail.com>
2024-11-03 11:24:38 +0100

    Handle login and logout

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -6,22 +6,25 @@ const UserContext = createContext()
 export function UserProvide({ children }) {
     const [user, setUser] = useState({})
 
+    const handleUserLogin = async () => {
+        const user = await axios.get("http://127.0.0.1:3000/users/user", {
+            withCredentials: true
+        }) 
+        setUser(user.data)
+    }
+
+    const handleUserLogout = async () => {
+        await axios.get("http://127.0.0.1:3000/users/logout", {
+            withCredentials: false
+        })
+        setUser({})
+    }
+
     return (
-        <UserContext.Provider value={user}>
+        <UserContext.Provider value={{user, handleUserLogin, handleUserLogout}}>
             { children }
         </UserContext.Provider>
     )
-}
-
-export async function handleUserLogin() {
-    const user = await axios.get("http://127.0.0.1:3000/users/user", {
-        withCredentials: true
-    }) 
-    setUser(user.data)
-}
-
-export async function handleUserLogout() {
-    setUser({})
 }
 
 export function useUser() {
